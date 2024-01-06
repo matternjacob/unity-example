2b50b8066c1fd46686dac22643ad872f62585653
matternjacob <123212978+matternjacob@users.noreply.github.com>
2024-01-05 23:41:39 -0600

    Added optimized movement, fixed infinite jump issue, added both running and running transition animation.

--- Unity Example/Assets/SampleScenesAssets/Scripts/Player Scripts/PlayerMovement.cs
+++ Unity Example/Assets/SampleScenesAssets/Scripts/Player Scripts/PlayerMovement.cs
@@ -1,78 +1,94 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor.Tilemaps;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
-    private bool isFacingRight = true;
+    private bool isFacingRight = true;     
     private float horizontal;
-    public float jumpingPower = 12f;
+    public float jumpingPower;
+    private bool isGrounded;
     bool isJumping;
-    public GameObject thePlayer;
+    public Animator animator;
 
-    [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] CharacterController controller;
+    [SerializeField] Vector2 moveDirection;
 
-    void Start()
+
+    void Start() //Rigid body pull
     {
         rb = GetComponent<Rigidbody2D>();
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
 
-        if (Input.GetKeyDown("Space"))
-        {
-            thePlayer.GetComponent<Animator>().Play("Bob Jump-Sheet_0");
-        }
     }
 
-    void Update()
+
+    void Update() //Horizontal and Jumping Functions
     {
-
-        
-
-        //horizontal Movement
         horizontal = Input.GetAxisRaw("Horizontal");
 
-        //Jump System
-        if (Input.GetButtonDown("Jump") && isGrounded())
+        rb.velocity = new Vector3(horizontal * speed, rb.velocity.y, 0);
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
+
+        //Jumping Code
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
-            
+            rb.velocity = Vector2.up * jumpingPower;
+            isJumping = true;
         }
+
+        //Checks if player is in the air
+        if (!isGrounded)
+        {
+            isJumping = true;
+        }
+
         Flip();
-    }
 
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Ground"))
+    }
+            private void OnCollisionEnter2D(Collision2D collision)
+            {
+                isGrounded = true;
+            }
+
+            private void OnCollisionExit2D(Collision2D collision)
+            {
+                isGrounded = false;
+            }
+
+
+     void FixedUpdate() //if player is not grounded, close the function 
+    { 
+        if (!isGrounded)
         {
-         
+              return;
         }
     }
 
-
-    private void FixedUpdate()
+    private void Flip() //player flip when facing in direction of movement
     {
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
-    }
 
-    //Ground Check
-    private bool isGrounded()
-    {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
-    }
-
-    //Character Flip On Direction Change
-    private void Flip()
-    {
         if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
         {
-            isFacingRight = !isFacingRight;
-            Vector3 localScale = transform.localScale;
-            localScale.x *= -1f;
-            transform.localScale = localScale;
+           isFacingRight = !isFacingRight;
+           Vector3 localScale = transform.localScale;
+           localScale.x *= -1f;
+           transform.localScale = localScale;
         }
 
+        
     }
+
+   
 }
+
